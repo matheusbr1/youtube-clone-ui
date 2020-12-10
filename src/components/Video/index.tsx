fbc1660d812b7779823ecf800ce40c586f4dbb3f
stefanini <=>
2020-12-09 23:09:14 -0300

    Adjust on video image

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -5,12 +5,10 @@ const Video: React.FC = () => {
     return (
         <Container>
             
-            <Thumb src="https://via.placeholder.com/300/F4F4F4/000000/?text=thumb" >Thumb</Thumb>
-            
+            <Thumb src="https://via.placeholder.com/300/F4F4F4/000000/?text=thumb" />
+
             <VideoInfosContainer>
-                <UserAvatar>
-                    <img src="https://yt3.ggpht.com/a-/AOh14GhTiYCPTFR0cQWjvKynpzti-6btplRKWQTlzuMbtF4=s108-c-k-c0x00ffffff-no-rj" alt=""/>
-                </UserAvatar>
+                <UserAvatar src="https://yt3.ggpht.com/a-/AOh14GhTiYCPTFR0cQWjvKynpzti-6btplRKWQTlzuMbtF4=s108-c-k-c0x00ffffff-no-rj" />
                 <Infos>
                     <p>Hard Rock Music | Best Hard Rock Songs Of 70s 80s 90s</p>
                     <span>Music Collection</span>
--- src/components/Video/style.ts
+++ src/components/Video/style.ts
@@ -13,25 +13,20 @@ export const Container = styled.div`
 `
 
 export const Thumb = styled.img`
-
     display: flex;
     align-items: center;
     justify-content: center;
 
-    min-height: 120px;
-    background: rgba(200, 200, 200, 0.2);
+    height: 184px;
+    width: 328px;
 
-    @media (max-width: 660px) {
-        min-height: 180px;
-    }
+    background: rgba(200, 200, 200, 0.2);
 `
-export const UserAvatar = styled.div`
-    img {
-        height: 32px;
-        width: 32px;
-        border-radius: 50%;
-        margin-right: 12px;
-    }
+export const UserAvatar = styled.img`
+    height: 32px;
+    width: 32px;
+    border-radius: 50%;
+    margin-right: 12px;
 `
 
 export const VideoInfosContainer = styled.div`
